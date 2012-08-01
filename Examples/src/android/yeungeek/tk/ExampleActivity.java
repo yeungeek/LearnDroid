8dad02ba75e5270504d57fdc78115369d62e1a3a
Anson.Yang <Anson.Yang@BJB0282.sky-mobi.com>
2012-08-01 18:45:56 +0800

    popupwindow效果

--- Examples/src/android/yeungeek/tk/ExampleActivity.java
+++ Examples/src/android/yeungeek/tk/ExampleActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.yeungeek.tk.pinnedheader.PinnedHeaderActivity;
+import android.yeungeek.tk.popupwindow.PopupWindowActivity;
 
 public class ExampleActivity extends Activity {
     /** Called when the activity is first created. */
@@ -18,6 +19,16 @@ public class ExampleActivity extends Activity {
         setContentView(R.layout.main);
 
         Button mPinnedBtn = (Button) findViewById(R.id.example_pinnedheader);
+        Button mPopupBtn = (Button) findViewById(R.id.example_popupwindow);
+
+        mPopupBtn.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ExampleActivity.this, PopupWindowActivity.class);
+                startActivity(intent);
+            }
+        });
+
         mPinnedBtn.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
